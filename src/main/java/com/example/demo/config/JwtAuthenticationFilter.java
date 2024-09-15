480e907509ca2bf58bb1de82607451b1889fdd21
Nguyễn Văn Tiệp <nguyenvantiep15112002@gmail.com>
2024-09-16 01:50:55 +0700

    up code xử lý hết hạn token

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import com.example.demo.model.utilities.JwtDecoder;
 import com.example.demo.model.utilities.JwtToPrincipalConverter;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,13 +26,22 @@ public class  JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        extractTokenFromRequest(request)
-                .map(jwtDecoder::decode)
-                .map(jwtToPrincipalConverter::convert)
-                .map(UserPrincipalAuthenticationToken::new)
-                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+        try {
+            extractTokenFromRequest(request)
+                    .map(jwtDecoder::decode)
+                    .map(jwtToPrincipalConverter::convert)
+                    .map(UserPrincipalAuthenticationToken::new)
+                    .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+
 
         filterChain.doFilter(request, response); // Very important not to forget this
+        }catch (ExpiredJwtException ex) {  // Bắt lỗi token hết hạn
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);  // Trả về 401 Unauthorized
+            response.getWriter().write("Token đã hết hạn");
+        } catch (JwtException ex) { // Bắt các lỗi khác liên quan đến JWT
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Token không hợp lệ");
+        }
     }
 
     private Optional<String> extractTokenFromRequest(HttpServletRequest request) {
